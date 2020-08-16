dd8cca6cc724651ecb99c7c09ea7f0926a750351
aconstlink <aconstlink@gmail.com>
2020-08-16 05:36:40 +0200

    natus update and database example update

--- 10_database/main.cpp
+++ 10_database/main.cpp
@@ -7,11 +7,11 @@
 using namespace natus::core::types ;
 
 //
-// this test packs a natus file from a file system
+// this test packs a natus file from a file system into same directory
 //
 int main( int argc, char ** argv )
 {
-    //natus::log::global_t::status( natus::std::string_t(DATAPATH) ) ;
+    natus::io::monitor_res_t mon = natus::io::monitor_t() ;
 
     natus::io::database db( natus::io::path_t( DATAPATH ) / "data" ) ;
 
@@ -30,7 +30,7 @@ int main( int argc, char ** argv )
     }
 
     {
-        db.load( "data2" ).wait_for_operation( [&] ( char_cptr_t data, size_t const sib, natus::io::result const res )
+        db.load( "noexist" ).wait_for_operation( [&] ( char_cptr_t data, size_t const sib, natus::io::result const res )
         {
             if( res != natus::io::result::ok )
             {
@@ -57,10 +57,31 @@ int main( int argc, char ** argv )
         } ) ;
     }
 
-    //db.pack() ;
-    ::std::this_thread::sleep_for( ::std::chrono::milliseconds(10000) ) ;
+    db.attach( mon ) ;
 
-    //db.dump_to_std() ;
+    // attach individually.
+    //db.attach( "data", mon ) ;
+    //db.attach( "subfolder.data", mon ) ;
+
+    db.pack() ;
+
+    // test file monitoring
+    // increase max_iter here and just save some of the txt files.
+    size_t const max_iter = 0 ;
+    for( size_t i=0; i<max_iter; ++i )
+    {
+        mon->for_each_and_swap( [&]( natus::std::string_cref_t loc, natus::io::monitor_t::notify const n )
+        {
+            natus::log::global_t::status( "[monitor] : Got " + natus::io::monitor_t::to_string(n) + " for " + loc ) ;
+        }) ;
+
+        ::std::this_thread::sleep_for( ::std::chrono::milliseconds(1000) ) ;
+    }
+
+    db.detach( mon ) ;
+
+    // dump content
+    db.dump_to_std() ;
 
     return 0 ;
 }
