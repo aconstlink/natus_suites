fe8656f48cbc97f907f6452813b53744259d431a
aconstlink <aconstlink@gmail.com>
2021-04-01 00:03:42 +0200

    added empty test file for empty app

---
+++ 00_empty/main.cpp
@@ -0,0 +1,75 @@
+
+#include "main.h"
+
+#include <natus/application/global.h>
+#include <natus/application/app.h>
+#include <natus/gfx/camera/pinhole_camera.h>
+#include <natus/graphics/variable/variable_set.hpp>
+#include <natus/math/vector/vector3.hpp>
+#include <natus/math/vector/vector4.hpp>
+#include <natus/math/matrix/matrix4.hpp>
+
+#include <thread>
+
+namespace this_file
+{
+    using namespace natus::core::types ;
+
+    class test_app : public natus::application::app
+    {
+        natus_this_typedefs( test_app ) ;
+
+    private:
+
+        app::window_async_t _wid_async ;
+
+    public:
+
+        test_app( void_t ) noexcept
+        {
+            natus::application::app::window_info_t wi ;
+            #if 0
+            _wid_async = this_t::create_window( "A Render Window", wi ) ;
+            #else
+            _wid_async = this_t::create_window( "A Render Window", wi, 
+                { natus::graphics::backend_type::gl3, natus::graphics::backend_type::d3d11 } ) ;
+            #endif
+        }
+        test_app( this_cref_t ) = delete ;
+        test_app( this_rref_t rhv ) noexcept : app( ::std::move( rhv ) ) 
+        {
+            _wid_async = std::move( rhv._wid_async ) ;
+        }
+        virtual ~test_app( void_t ) 
+        {}
+
+    private:
+
+        virtual natus::application::result on_init( void_t )
+        { 
+            return natus::application::result::ok ; 
+        }
+
+        virtual natus::application::result on_update( natus::application::app_t::update_data_in_t ) 
+        { 
+            return natus::application::result::ok ; 
+        }
+
+        virtual natus::application::result on_graphics( natus::application::app_t::render_data_in_t ) 
+        { 
+            return natus::application::result::ok ; 
+        }
+
+        virtual natus::application::result on_shutdown( void_t ) 
+        { 
+            return natus::application::result::ok ; 
+        }
+    };
+    natus_res_typedef( test_app ) ;
+}
+
+int main( int argc, char ** argv )
+{
+    return natus::application::global_t::create_application( 
+        this_file::test_app_res_t( this_file::test_app_t() ) )->exec() ;
+}
