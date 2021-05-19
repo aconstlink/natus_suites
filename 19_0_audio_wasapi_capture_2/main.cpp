f14c998b4af622841d94535073207a9c92a2e84e
aconstlink <aconstlink@gmail.com>
2021-05-19 21:04:19 +0200

    new test for the wasapi system audio mix capture

---
+++ 19_0_audio_wasapi_capture_2/main.cpp
@@ -0,0 +1,75 @@
+
+#include "main.h"
+
+#include <natus/ntd/string.hpp>
+#include <natus/log/global.h>
+#include <natus/ntd/vector.hpp>
+#include <natus/math/vector/vector2.hpp>
+
+#include <cstdlib>
+#include <chrono>
+#include <limits>
+#include <thread>
+#include <functional>
+
+#include <natus/audio/backend/wasapi/wasapi_capture_helper.h>
+
+using namespace natus::core::types ;
+
+//
+// - test audio capture with wasapi loopback device
+// using the internal helper
+//
+int main( int argc, char ** argv )
+{
+
+    natus::ntd::vector< float_t > samples ;
+
+    natus::audio::wasapi_capture_helper_t hlp ;
+
+    {
+        hlp.init() ;
+
+        typedef std::chrono::high_resolution_clock clock_t ;
+        clock_t::time_point tp = clock_t::now() ;
+
+        while( std::chrono::duration_cast<std::chrono::seconds>( clock_t::now() - tp ) < std::chrono::seconds( 3 ) )
+        {
+            hlp.capture( samples ) ;
+
+            // print data
+            {
+                float_t accum = 0.0f ;
+                for( auto const s : samples ) accum += s ;
+
+                natus::log::global_t::status( std::to_string(accum / float_t(samples.size()))  ) ;
+            }
+        }
+
+        hlp.release() ;
+    }
+    
+    {
+        hlp.init() ;
+
+        typedef std::chrono::high_resolution_clock clock_t ;
+        clock_t::time_point tp = clock_t::now() ;
+
+        while( std::chrono::duration_cast<std::chrono::seconds>( clock_t::now() - tp ) < std::chrono::seconds( 10 ) )
+        {
+            hlp.capture( samples ) ;
+
+            // print data
+            {
+                float_t accum = 0.0f ;
+                for( auto const s : samples ) accum += s ;
+
+                natus::log::global_t::status( std::to_string(accum / float_t(samples.size()))  ) ;
+            }
+        }
+
+        hlp.release() ;
+    }
+
+    return 0 ;
+}
