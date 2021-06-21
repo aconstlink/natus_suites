5bfe00b48bcc91a8b58404f5b0fa330dcd822ded
aconstlink <aconstlink@gmail.com>
2021-06-21 23:48:09 +0200

    :alien:

--- 02_profile/main.cpp
+++ 02_profile/main.cpp
@@ -7,10 +7,8 @@
 
 using namespace natus::core::types ;
 
-//
-int main( int argc, char ** argv )
+void_t test_local_system()
 {
-
     natus::profile::system_t psys ;
 
     {
@@ -18,7 +16,7 @@ int main( int argc, char ** argv )
 
         for( size_t i=0; i<1000000; ++i ) {}
 
-        psys.make_entry( std::move( e ) ) ;
+        psys.add_entry( std::move( e ) ) ;
     }
 
     {
@@ -26,11 +24,11 @@ int main( int argc, char ** argv )
 
         for( size_t i=0; i<1000000; ++i ) {}
 
-        psys.make_entry( std::move( e ) ) ;
+        psys.add_entry( std::move( e ) ) ;
     }
 
     {
-        natus::profile::entry_t e( "in_thread", "in_group", "my_unique" ) ;
+        natus::profile::entry_t e( "thread.group.my_unique" ) ;
 
         std::this_thread::sleep_for( std::chrono::milliseconds(100) ) ;
 
@@ -38,19 +36,50 @@ int main( int argc, char ** argv )
     }
 
     {
-        natus::profile::entry_t e( "in_thread", "in_group", "my_unique" ) ;
+        natus::profile::entry_t e( "thread.group.my_unique" ) ;
 
         std::this_thread::sleep_for( std::chrono::milliseconds(200) ) ;
 
         psys.unique_entry( std::move( e ) ) ;
     }
 
+    natus::log::global_t::status( "******************************** Local System ********************************** " ) ;
+
     {
         auto const entries = psys.get_entries() ;
         for( auto const & e : entries )
         {
-            natus::log::global_t::status( e.get_key() + " : " + std::to_string( e.get_duration<std::chrono::microseconds>().count() ) + " [micro]" ) ;
+            natus::log::global_t::status( e.get_name() + " : " + std::to_string( e.get_duration<std::chrono::microseconds>().count() ) + " [micro]" ) ;
         }
     }
+}
+
+void_t test_global_system()
+{
+    natus::log::global_t::status( "******************************** Global System ********************************** " ) ;
+    {
+        natus::profile::entry_t e( "thread.group.my_unique" ) ;
+
+        std::this_thread::sleep_for( std::chrono::milliseconds(200) ) ;
+
+        natus::profile::global_t::sys().unique_entry( std::move( e ) ) ;
+    }
+
+
+    {
+        auto const entries = natus::profile::global_t::sys().get_entries() ;
+        for( auto const & e : entries )
+        {
+            natus::log::global_t::status( e.get_name() + " : " + std::to_string( e.get_duration<std::chrono::microseconds>().count() ) + " [micro]" ) ;
+        }
+    }
+}
+
+//
+int main( int argc, char ** argv )
+{
+    test_local_system() ;
+    test_global_system() ;
+
     return 0 ;
 }
