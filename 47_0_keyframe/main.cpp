6c09fb9c5a1bc49845533c5a854582e3396ecf08
aconstlink <aconstlink@gmail.com>
2022-07-11 23:26:32 +0200

    keyframe test

--- 47_0_keyframe/main.cpp
+++ 47_0_keyframe/main.cpp
@@ -1,6 +1,8 @@
 
 #include <natus/math/vector/vector2.hpp>
 #include <natus/math/animation/linear_keyframe_sequence.hpp>
+#include <natus/math/animation/keyframe_sequence.hpp>
+#include <natus/math/spline/cubic_hermit_spline.hpp>
 
 #include <natus/log/global.h>
 
@@ -32,6 +34,23 @@ int main( int argc, char ** argv )
         natus::log::global_t::status( std::to_string( kf( 1000 ).x() ) + ":" + std::to_string( kf( 1000 ).y() ) ) ;
         natus::log::global_t::status( std::to_string( kf( 2000 ).x() ) + ":" + std::to_string( kf( 2000 ).y() ) ) ;
         natus::log::global_t::status( std::to_string( kf( 3000 ).x() ) + ":" + std::to_string( kf( 3000 ).y() ) ) ;
+    }
+
+    {
+        typedef natus::math::cubic_hermit_spline<float_t> spline_t ;
+        typedef natus::math::keyframe_sequence< spline_t > keyframe_sequence_t ;
+
+        keyframe_sequence_t kf ;
+        kf.insert( keyframe_sequence_t::keyframe_t( 0, 0.0f ) ) ;
+        kf.insert( keyframe_sequence_t::keyframe_t( 1000, 1.0f ) ) ;
+        kf.insert( keyframe_sequence_t::keyframe_t( 4000, 25.0f ) ) ;
+        kf.insert( keyframe_sequence_t::keyframe_t( 2000, 10.0f ) ) ;
+
+        natus::log::global_t::status( std::to_string( kf( 0 ) ) ) ;
+        natus::log::global_t::status( std::to_string( kf( 1000 ) ) ) ;
+        natus::log::global_t::status( std::to_string( kf( 2000 ) ) ) ;
+        natus::log::global_t::status( std::to_string( kf( 3000 ) ) ) ;
+        natus::log::global_t::status( std::to_string( kf( 4000 ) ) ) ;
     }
 
     return 0 ;
