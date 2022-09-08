a33533f72642096cba14b60a6b4644f4c33aa6c0
aconstlink <aconstlink@gmail.com>
2022-09-08 14:07:30 +0200

    essential test

---
+++ 50_simple_essentials/main.cpp
@@ -0,0 +1,124 @@
+
+
+#include <natus/application/global.h>
+#include <natus/application/app.h>
+#include <natus/tool/imgui/custom_widgets.h>
+
+#include <natus/application/util/simple_app_essentials.h>
+
+#include <natus/profile/macros.h>
+
+#include <random>
+#include <thread>
+
+//
+namespace this_file
+{
+    using namespace natus::core::types ;
+
+    class test_app : public natus::application::app
+    {
+        natus_this_typedefs( test_app ) ;
+
+    private:
+
+        natus::application::util::simple_app_essentials_t _ae ;
+
+    private: //
+
+        natus::graphics::async_views_t _graphics ;
+
+    private:
+
+    public:
+
+        test_app( void_t ) 
+        {
+            natus::application::app::window_info_t wi ;
+            #if 1
+            auto view1 = this_t::create_window( "A Render Window Default", wi ) ;
+            auto view2 = this_t::create_window( "A Render Window Additional", wi,
+                { natus::graphics::backend_type::gl3, natus::graphics::backend_type::d3d11}) ;
+
+            view1.window().position( 50, 50 ) ;
+            view1.window().resize( 800, 800 ) ;
+            view2.window().position( 50 + 800, 50 ) ;
+            view2.window().resize( 800, 800 ) ;
+
+            _graphics = natus::graphics::async_views_t( { view1.async(), view2.async() } ) ;
+            #else
+            auto view1 = this_t::create_window( "A Render Window", wi, 
+                { natus::graphics::backend_type::gl3, natus::graphics::backend_type::d3d11 } ) ;
+            _graphics = natus::graphics::async_views_t( { view1.async() } ) ;
+            #endif
+
+            //_db = natus::io::database_t( natus::io::path_t( DATAPATH ), "./working", "data" ) ;
+        }
+        test_app( this_cref_t ) = delete ;
+        test_app( this_rref_t rhv ) noexcept : app( ::std::move( rhv ) ) 
+        {
+            _ae = std::move( rhv._ae ) ;
+        }
+        virtual ~test_app( void_t ) 
+        {}
+
+        virtual natus::application::result on_event( window_id_t const wid, this_t::window_event_info_in_t wei ) noexcept
+        {
+            _ae.on_event( wid, wei ) ;
+            return natus::application::result::ok ;
+        }        
+
+    private:
+
+        virtual natus::application::result on_init( void_t ) noexcept
+        { 
+            _ae.init_graphics( "myapp", _graphics ) ;
+            return natus::application::result::ok ; 
+        }
+
+        //*****************************************************************************
+        virtual natus::application::result on_device( device_data_in_t ) noexcept 
+        { 
+            return natus::application::result::ok ; 
+        }
+
+        //*****************************************************************************
+        virtual natus::application::result on_update( natus::application::app_t::update_data_in_t ud ) noexcept 
+        { 
+            NATUS_PROFILING_COUNTER_HERE( "Update Clock" ) ;
+            return natus::application::result::ok ; 
+        }
+
+        //*****************************************************************************
+        virtual natus::application::result on_physics( natus::application::app_t::physics_data_in_t ud ) noexcept
+        {
+            return natus::application::result::ok ; 
+        }
+
+        //*****************************************************************************
+        virtual natus::application::result on_graphics( natus::application::app_t::render_data_in_t rd ) noexcept 
+        {
+            _ae.on_graphics_begin( rd ) ;
+
+            _ae.on_graphics_end( 100 ) ;
+
+            return natus::application::result::ok ; 
+        }
+
+        virtual natus::application::result on_tool( natus::application::app::tool_data_ref_t ) noexcept
+        {
+            if( false ) return natus::application::result::no_imgui ;
+            return natus::application::result::ok ;
+        }
+
+        virtual natus::application::result on_shutdown( void_t ) noexcept 
+        { return natus::application::result::ok ; }
+    };
+    natus_res_typedef( test_app ) ;
+}
+
+int main( int argc, char ** argv )
+{
+    return natus::application::global_t::create_application( 
+        this_file::test_app_res_t( this_file::test_app_t() ) )->exec() ;
+}
