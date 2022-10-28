60c4e0a5e642b9745b4d52f76c666bbe5ad0891c
aconstlink <aconstlink@gmail.com>
2022-10-29 01:02:44 +0200

    more testing

--- 16_nsl/main.cpp
+++ 16_nsl/main.cpp
@@ -423,17 +423,55 @@ void_t test_4( natus::io::database_res_t db )
     }
 }
 
+void_t test_5( natus::io::database_res_t db )
+{
+    natus::nsl::database_res_t ndb = natus::nsl::database_t() ;
+    natus::ntd::vector< natus::io::location_t > shader_locations = {
+        natus::io::location_t( "shaders.main_early_return.nsl" )
+    };
+
+    natus::ntd::vector< natus::nsl::symbol_t > config_symbols ;
+
+    for( auto const& l : shader_locations )
+    {
+        natus::format::module_registry_res_t mod_reg = natus::format::global_t::registry() ;
+        auto fitem2 = mod_reg->import_from( l, db ) ;
+
+        natus::format::nsl_item_res_t ii = fitem2.get() ;
+        if( ii.is_valid() ) ndb->insert( std::move( std::move( ii->doc ) ), config_symbols ) ;
+    }
+    
+    for( auto const & c : config_symbols )
+    {
+        natus::nsl::generatable_t res = natus::nsl::dependency_resolver_t().resolve( ndb, c ) ;
+        if( res.missing.size() != 0 )
+        {
+            natus::log::global_t::warning( "We have missing symbols." ) ;
+            for( auto const& s : res.missing )
+            {
+                natus::log::global_t::status( s.expand() ) ;
+            }
+        }
+
+        {
+            auto code = natus::nsl::generator_t( std::move( res ) ).generate() ;
+            int const bp = 0 ;
+        }
+    }
+}
+
 int main( int argc, char ** argv )
 {
     natus::io::database_res_t db = natus::io::database_t( natus::io::path_t( DATAPATH ), "./working", "data" ) ;
 
     //test_1() ;
     //test_1_1_extract_by_tokenize() ;
-    test_1_2() ;
+    //test_1_2() ;
     //test_2( db ) ;
     //test_3( db ) ;
 
     //test_4( db ) ;
+    test_5( db ) ;
 
     return 0 ;
 }
