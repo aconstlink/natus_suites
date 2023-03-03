29c6c25619e7920e1c7c9d057c218c7466450810
aconstlink <aconstlink@gmail.com>
2023-03-03 15:14:06 +0100

    more physics

--- suite_physics/phy_1_0_collision/main.cpp
+++ suite_physics/phy_1_0_collision/main.cpp
@@ -14,17 +14,148 @@
 #include <natus/physics/particle_system.h>
 #include <natus/physics/force_fields.hpp>
 
+#include <natus/collide/2d/bounds/aabb.hpp>
+#include <natus/collide/2d/bounds/circle.hpp>
+#include <natus/collide/2d/hit_test/hit_test_aabb_circle.hpp>
+
 #include <random>
 #include <thread>
-
-
 
 namespace this_file
 {
     using namespace natus::core ;
-
     using namespace natus::core::types ;
 
+    //****************************************************************************************
+    class physic_property
+    {
+        // orientation
+        
+        float_t _mass = 1.0f ;
+        natus::math::vec2f_t _pos ;
+        natus::math::vec2f_t _vel ;
+        natus::math::vec2f_t _force ;
+
+    public:
+
+        natus::math::vec2f_cref_t get_force( void_t ) const noexcept{ return _force ; }
+        void_t set_force( natus::math::vec2f_cref_t f ) noexcept{ _force = f ; }
+        void_t add_force( natus::math::vec2f_cref_t f ) noexcept{ _force += f ; }
+
+        natus::math::vec2f_cref_t get_velocity( void_t ) const noexcept{ return _vel ; }
+        void_t set_velocity( natus::math::vec2f_cref_t v ) noexcept{ _vel = v ; }
+
+        natus::math::vec2f_cref_t get_position( void_t ) const noexcept{ return _pos ; }
+        void_t set_position( natus::math::vec2f_cref_t v ) noexcept{ _pos = v ; }
+
+        void_t reset_force( void_t ) noexcept { _force = natus::math::vec2f_t() ; }
+
+        void_t integrate( float_t const dt, float_t const ppm ) noexcept
+        {
+            auto const a = (_force * ppm) / _mass ;
+            _vel = _vel + a * dt ;
+            _pos = _pos + _vel * dt ;
+        }
+    };
+    natus_typedef( physic_property ) ;
+
+    //****************************************************************************************
+    class shape_property
+    {
+    };
+    natus_typedef( shape_property ) ;
+
+    //****************************************************************************************
+    class object
+    {
+        physic_property_t _phy_prop ;
+        shape_property_t _shp_prop ;
+
+    public:
+
+        physic_property_cref_t get_physic( void_t ) const noexcept { return _phy_prop ; }
+        physic_property_ref_t get_physic( void_t ) noexcept { return _phy_prop ; }
+
+    };
+    natus_typedef( object ) ;
+
+    //****************************************************************************************
+    // shoots objects
+    class object_cannon
+    {
+        natus::math::vec2f_t _position ;
+        natus::math::vec2f_t _direction = natus::math::vec2f_t(-100.0f, 0.0f ) ;
+        float_t _mag = 10.0f ;
+
+    public:
+
+        natus::math::vec2f_t get_position( void_t ) const noexcept
+        {
+            return _position ;
+        }
+
+        natus::math::vec2f_t get_direction( void_t ) const noexcept
+        {
+            return _direction ;
+        }
+
+        natus::math::vec2f_t gen_velocity( void_t ) const noexcept
+        {
+            return _direction ;
+        }
+
+        natus::math::vec2f_t gen_force( void_t ) const noexcept
+        {
+            return _direction.normalized() * _mag ;
+        }
+
+    public:
+
+        void_t set_direction( natus::math::vec2f_cref_t dir ) noexcept
+        {
+            _direction = dir ;
+        }
+
+        void_t set_position( natus::math::vec2f_cref_t pos ) noexcept
+        {
+            _position = pos ;
+        }
+
+        void_t add_magnitude( float_t const f ) noexcept { _mag += f ; }
+        void_t set_magnitude( float_t const f ) noexcept { _mag = f ; }
+
+    public:
+
+        this_file::object_t gen_object( void_t ) const noexcept
+        {
+            auto const o = this_file::object_t() ;
+
+            return o ;
+        }
+    };
+    natus_typedef( object_cannon ) ;
+
+    //****************************************************************************************
+    class ground_object
+    {
+    public:
+        natus_typedefs( natus::collide::n2d::aabb<float_t>, box ) ;
+
+    private:
+
+        box_t _bound ;
+
+    public:
+
+        box_t get_box( void_t ) const noexcept { return _bound ; }
+        void_t set_box( box_cref_t b ) noexcept { _bound = b ; }
+
+    };
+    natus_typedef( ground_object ) ;
+
+    //****************************************************************************************
+    //
+    //
     class test_app : public natus::application::app
     {
         natus_this_typedefs( test_app ) ;
@@ -32,6 +163,16 @@ namespace this_file
     private:
 
         natus::application::util::app_essentials_t _ae ;
+
+    private:
+
+        object_cannon_t _obj_cannon ;
+        natus::ntd::vector< ground_object_t > _ground ;
+        natus::ntd::vector< object_t > _objects ;
+        natus::collide::n2d::aabbf_t _bound ;
+        
+        // pixel per meter
+        float_t _ppm = 50.0f ;
 
     public:
 
@@ -67,6 +208,7 @@ namespace this_file
         virtual natus::application::result on_event( window_id_t const wid, this_t::window_event_info_in_t wei ) noexcept
         {
             _ae.on_event( wid, wei ) ;
+            _ae.get_camera_0()->orthographic() ;
             return natus::application::result::ok ;
         }
 
@@ -81,7 +223,58 @@ namespace this_file
             } ;
 
             _ae.init( is ) ;
-            
+            _ae.enable_mouse_control( false ) ;
+
+            // init ground obstacles
+            {
+                {
+                    this_file::ground_object_t go ;
+                    auto const b = this_file::ground_object_t::box_t( natus::math::vec2f_t(-400.0f, -500.0f), natus::math::vec2f_t(500.0f, -300.0f) );
+                    go.set_box( b ) ;
+                    _ground.emplace_back( go ) ;
+                }
+
+                {
+                    this_file::ground_object_t go ;
+                    go.set_box( this_file::ground_object_t::box_t( natus::math::vec2f_t(-400.0f, -400.0f), natus::math::vec2f_t(-350.0f, 400.0f) ) ) ;
+                    _ground.emplace_back( go ) ;
+                }
+
+                {
+                    this_file::ground_object_t go ;
+                    go.set_box( this_file::ground_object_t::box_t( natus::math::vec2f_t(-120.0f, -120.0f), natus::math::vec2f_t( -90.0f, -90.0f) ) ) ;
+                    _ground.emplace_back( go ) ;
+                }
+
+                {
+                    this_file::ground_object_t go ;
+                    go.set_box( this_file::ground_object_t::box_t( natus::math::vec2f_t(-400.0f, 200.0f), natus::math::vec2f_t( -50.0f, 230.0f) ) ) ;
+                    _ground.emplace_back( go ) ;
+                }
+
+                {
+                    this_file::ground_object_t go ;
+                    go.set_box( this_file::ground_object_t::box_t( natus::math::vec2f_t(100.0f, -400.0f), natus::math::vec2f_t( 250.0f, -100.0f) ) ) ;
+                    _ground.emplace_back( go ) ;
+                }
+
+                {
+                    this_file::ground_object_t go ;
+                    go.set_box( this_file::ground_object_t::box_t( natus::math::vec2f_t(400.0f, -400.0f), natus::math::vec2f_t( 450.0f, 400.0f) ) ) ;
+                    _ground.emplace_back( go ) ;
+                }
+
+                {
+                    this_file::ground_object_t go ;
+                    go.set_box( this_file::ground_object_t::box_t( natus::math::vec2f_t(0.0f, -300.0f), natus::math::vec2f_t( 50.0f, -200.0f) ) ) ;
+                    _ground.emplace_back( go ) ;
+                }
+            }
+
+            {
+                _bound = natus::collide::n2d::aabbf_t( natus::math::vec2f_t(-1000.0f), natus::math::vec2f_t(1000.0f) ) ;
+            }
+
             return natus::application::result::ok ; 
         }
 
@@ -89,7 +282,45 @@ namespace this_file
 
         virtual natus::application::result on_device( device_data_in_t dd ) noexcept 
         { 
-           _ae.on_device( dd ) ;
+            _ae.on_device( dd ) ;
+
+            // mouse
+            {
+                natus::device::layouts::three_mouse_t mouse( _ae.get_mouse_dev() ) ;
+
+                if( mouse.is_pressing( natus::device::layouts::three_mouse::button::right ) )
+                {
+                    _obj_cannon.set_position( _ae.get_cur_mouse_pos() ) ;
+                }
+
+                if( mouse.is_pressing( natus::device::layouts::three_mouse::button::left ) )
+                {
+                    _obj_cannon.set_direction( _ae.get_cur_mouse_pos() - _obj_cannon.get_position() ) ;
+                }
+            }
+
+            // keyboard
+            {
+                natus::device::layouts::ascii_keyboard_t keyboard( _ae.get_ascii_dev() ) ;
+                
+                if( keyboard.get_state( natus::device::layouts::ascii_keyboard::ascii_key::space ) == 
+                    natus::device::components::key_state::pressing )
+                {
+                    _obj_cannon.add_magnitude( 100.0f ) ;
+                }
+                else if( keyboard.get_state( natus::device::layouts::ascii_keyboard::ascii_key::space ) == 
+                    natus::device::components::key_state::released )
+                {
+                    // shoot object
+                    this_file::object_t obj ;
+                    obj.get_physic().set_force( _obj_cannon.gen_force() ) ;
+                    obj.get_physic().set_position( _obj_cannon.get_position() ) ;
+                    _objects.emplace_back( obj ) ;
+
+                    _obj_cannon.set_magnitude( 10.0f ) ;
+                }
+            }
+
             return natus::application::result::ok ; 
         }
 
@@ -100,6 +331,54 @@ namespace this_file
 
         virtual natus::application::result on_physics( natus::application::app_t::physics_data_in_t ud ) noexcept
         {
+            // apply base force
+            // e.g. gravity, wind, air friction
+            {
+                for( auto & o : _objects )
+                {
+                    auto & phy = o.get_physic() ;
+                    phy.add_force( natus::math::vec2f_t( 0.0f, -9.81f ) ) ;
+                }
+            }
+
+            // apply collision force
+            // e.g friction, drag, impluse, rotation
+            {
+            }
+
+            // integrate
+            {
+                float_t const dt = ud.sec_dt ;
+                for( auto & o : _objects )
+                {
+                    auto & phy = o.get_physic() ;
+                    phy.integrate( dt, _ppm ) ;
+                }
+            }
+
+            // check bounds
+            {
+                for( size_t i=0; i<_objects.size(); ++i )
+                {
+                    auto & o = _objects[i] ;
+
+                    if( !_bound.is_inside( o.get_physic().get_position() )  )
+                    {
+                        _objects[i] = _objects[_objects.size()-1] ;
+                        _objects.resize( _objects.size()-1) ;
+                    }
+                }
+            }
+
+            // reset force
+            {
+                for( auto & o : _objects )
+                {
+                    auto & phy = o.get_physic() ;
+                    phy.reset_force() ;
+                }
+            }
+
             return natus::application::result::ok ; 
         }
 
@@ -109,7 +388,41 @@ namespace this_file
 
             auto pr = _ae.get_prim_render() ;
 
-            
+            // draw ground
+            {
+                for( auto const & g : _ground )
+                {
+                    natus::math::vec2f_t points[4] ;
+                    g.get_box().get_points( points ) ;
+                    pr->draw_rect( 5, points[0], points[1], points[2], points[3], 
+                        natus::math::vec4f_t( .1f, .1f, .1f, 1.0f ), natus::math::vec4f_t(.1f, .1f, .1f, 1.0f) ) ;
+                }
+            }
+
+            // draw cannon
+            {
+                {
+                    auto const p0 = _obj_cannon.get_position() ;
+                    auto const p1 = p0 + _obj_cannon.gen_force() * 0.1f;
+                    pr->draw_circle( 10, 10, p0, 10.0f, natus::math::vec4f_t(1.0f),natus::math::vec4f_t(1.0f) ) ;
+                    pr->draw_line( 10, p0, p1, natus::math::vec4f_t(1.0f, 0.0f, 0.0, 1.0f) ) ;
+                }
+
+                {
+                    auto const p0 = _obj_cannon.get_position() ;
+                    auto const p1 = p0 + _obj_cannon.gen_force().normalize() * 10.0f ;
+                    pr->draw_line( 11, p0, p1, natus::math::vec4f_t(0.0f, 1.0f, 0.0, 1.0f) ) ;
+                }
+            }
+
+            // draw objects
+            {
+                for( auto & o : _objects )
+                {
+                    auto const p0 = o.get_physic().get_position() ;
+                    pr->draw_circle( 11, 10, p0, 10.0f, natus::math::vec4f_t(1.0f),natus::math::vec4f_t(1.0f) ) ;
+                }
+            }
 
             _ae.on_graphics_end( 100 ) ;
 
